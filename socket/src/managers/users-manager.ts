c643d84162744bd8a709e1405dfafdeb1d229af7
Filipe Melo <filipe.melo@liven.tech>
2023-05-01 11:05:44 -0300

    feat(socket): add jwt auth

--- socket/src/managers/users-manager.ts
+++ socket/src/managers/users-manager.ts
@@ -1,6 +1,7 @@
-import { remove } from "lodash"
+import { pick, remove } from "lodash"
 import { Socket } from "socket.io"
-import { User, createUser } from "../models/user"
+import { CreateUserProps, User, createUser } from "../models/user"
+import { generateToken, validateToken } from "./auth-manager"
 
 const users: User[] = []
 
@@ -12,14 +13,19 @@ export const findUser = (userId: User["id"]) =>
   users.find(({ id }) => id === userId)
 
 const makeUser = (socket: Socket, name: string, token?: string) => {
-  //TODO: handle validate token
   if (token) {
-    // TODO: handle extract token to user object
-    const userProps = {} as User
-    return createUser(userProps)
-  } else {
-    return createUser({ name, socket })
+    const payload = validateToken(token)
+
+    if (!findUser(payload.id))
+      return createUser({
+        ...(pick(payload, ["id", "name"]) as CreateUserProps),
+        socket,
+      })
+
+    return createUser({ name: payload.name, socket })
   }
+
+  return createUser({ name, socket })
 }
 
 export const makeAuthMe = (
@@ -31,11 +37,8 @@ export const makeAuthMe = (
       const newUser = makeUser(socket, name, token)
       users.push(newUser)
 
-      if (!token) {
-        // TODO: create token for user reconnection
-        const refreshedToken = ""
-        // socket.send("refreshToken", refreshedToken)
-      }
+      const refreshedToken = generateToken(newUser)
+      socket.send("refreshToken", refreshedToken)
 
       socket.on("disconnect", () => {
         remove(users, (user) => user.id === newUser.id)
